0eba9185ceec381782c446f9f093dfde005f2aaa
Daniel Johnson <djohnson@squarespace.com>
2018-07-18 15:36:43 -0400

    Switches back to reset as LESS, build LESS files to CSS files without webpack loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import './styles/main.less'
-
 const myArray = new Array('foo', 'bar', 'baz',)
 myArray.forEach((item, index) => {
   console.log(`myArray[${index}] = ${item}`)
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,6 @@ module.exports = {
           loader: "babel-loader"
         }
       },
-      {
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
-      },
     ]
   }
 }
